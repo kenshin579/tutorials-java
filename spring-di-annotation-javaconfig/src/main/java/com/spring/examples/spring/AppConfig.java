f07320dd0a3262631a15286e6bc08abada12e929
Frank <frankyoh@tmon.co.kr>
2018-12-16 20:36:18 +0900

    updated javaconfig

--- spring-di-annotation-javaconfig/src/main/java/com/spring/examples/spring/AppConfig.java
+++ spring-di-annotation-javaconfig/src/main/java/com/spring/examples/spring/AppConfig.java
@@ -1,9 +1,15 @@
 package com.spring.examples.spring;
 
+import com.spring.examples.respository.QuoteRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.spring.examples")
 public class AppConfig {
+    @Bean
+    public QuoteRepository getQuoteRepository(QuoteRepository quoteRepository) {
+        return quoteRepository;
+    }
 }
