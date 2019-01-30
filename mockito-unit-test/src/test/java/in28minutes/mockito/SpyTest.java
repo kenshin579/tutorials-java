b86a141923386d3d73823221942da5688e75f581
Frank <frankyoh@tmon.co.kr>
2019-01-30 23:10:48 +0900

    finish studying step12

---
+++ mockito-unit-test/src/test/java/in28minutes/mockito/SpyTest.java
@@ -0,0 +1,42 @@
+package in28minutes.mockito;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SpyTest {
+
+	@Test
+	public void creatingASpyOnArrayList() {
+		List<String> listSpy = spy(ArrayList.class);
+		listSpy.add("Ranga");
+		listSpy.add("in28Minutes");
+
+		verify(listSpy).add("Ranga");
+		verify(listSpy).add("in28Minutes");
+
+		assertEquals(2, listSpy.size());
+		assertEquals("Ranga", listSpy.get(0));
+	}
+
+	@Test
+	public void creatingASpyOnArrayList_overridingSpecificMethods() {
+		List<String> listSpy = spy(ArrayList.class);
+		listSpy.add("Ranga");
+		listSpy.add("in28Minutes");
+
+		when(listSpy.size()).thenReturn(-1);
+		//		stub(listSpy.size()).toReturn(-1);
+
+		assertEquals(-1, listSpy.size());
+		assertEquals("Ranga", listSpy.get(0));
+
+	}
+
+}
