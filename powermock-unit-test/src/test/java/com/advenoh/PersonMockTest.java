8768dbe167780ad9912caaf7a3ed8c280998e9d6
Frank Oh <frankyoh@tmon.co.kr>
2019-01-30 14:21:38 +0900

    added personmock - not working

--- powermock-unit-test/src/test/java/com/advenoh/PersonMockTest.java
+++ powermock-unit-test/src/test/java/com/advenoh/PersonMockTest.java
@@ -1,17 +1,15 @@
 package com.advenoh;
 
-import com.advenoh.model.Person;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class PersonMockTest {
 
 	@Test
 	public void name() {
-		Person p = mock(Person.class);
+		Child p = mock(Child.class);
 		Assertions.assertThat(p).isNotNull();
 	}
 }
