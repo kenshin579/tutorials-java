02395ac2f892ebb05340b170cde8b384927ccaea
Frank <frankyoh@tmon.co.kr>
2019-01-30 22:48:45 +0900

    finish studying step06

--- mockito-unit-test/src/test/java/in28minutes/TodoBusinessImplMockitoTest.java
+++ mockito-unit-test/src/test/java/in28minutes/TodoBusinessImplMockitoTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -27,4 +30,21 @@ public class TodoBusinessImplMockitoTest {
 
 		verify(todoService).retrieveTodos("Ranga");
 	}
+
+	@Test
+	public void usingMockito_UsingBDD() {
+		TodoService todoService = mock(TodoService.class);
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+
+		//given
+		given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
+
+		//when
+		List<String> todos = todoBusinessImpl.retrieveTodosRelatedToSpring("Ranga");
+
+		//then
+		assertThat(todos.size(), is(2));
+	}
+
 }
