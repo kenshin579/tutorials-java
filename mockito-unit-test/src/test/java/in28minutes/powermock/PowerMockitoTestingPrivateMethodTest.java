dac3a87ee23e99e870e25869271a82bec9c79c74
Frank Oh <frankyoh@tmon.co.kr>
2019-01-31 15:41:44 +0900

    studying step15

---
+++ mockito-unit-test/src/test/java/in28minutes/powermock/PowerMockitoTestingPrivateMethodTest.java
@@ -0,0 +1,31 @@
+package in28minutes.powermock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+public class PowerMockitoTestingPrivateMethodTest {
+
+	@Mock
+	Dependency dependencyMock;
+
+	@InjectMocks
+	SystemUnderTest systemUnderTest;
+
+	@Test
+	public void powerMockito_CallingAPrivateMethod() throws Exception {
+		when(dependencyMock.retrieveAllStats()).thenReturn(Arrays.asList(1, 2, 3));
+
+		long value = Whitebox.invokeMethod(systemUnderTest, "privateMethodUnderTest");
+		assertEquals(6, value);
+	}
+}
