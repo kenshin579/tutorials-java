86260ac6601e7a14a73495e9783afaddd04dc6dc
Frank Oh <frankyoh@tmon.co.kr>
2019-02-14 18:21:55 +0900

    added example for rabbitmq example

---
+++ springboot-messaging-rabbitmq/src/main/java/hello/Receiver.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+
+	private CountDownLatch latch = new CountDownLatch(1);
+
+	public void receiveMessage(String message) {
+		System.out.println("Received <" + message + ">");
+		latch.countDown();
+	}
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+}
