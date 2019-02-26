769f87bcc57e083d06087cefa9f63e6251199fd0
Frank Oh <frankyoh@tmon.co.kr>
2019-02-26 11:09:37 +0900

    update sources for junit rule

--- junit-rule/src/test/java/com/advenoh/SpringContextRuleTest.java
+++ junit-rule/src/test/java/com/advenoh/SpringContextRuleTest.java
@@ -4,9 +4,15 @@ import com.advenoh.rules.SpringContextRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class SpringContextRuleTest {
 
@@ -14,11 +20,23 @@ public class SpringContextRuleTest {
 	public TestRule contextRule = new SpringContextRule(
 			new String[] { "testContext.xml" }, this);
 
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+
 	@Autowired
 	public String bar;
 
+	@Mock
+	public List baz;
+
+	@Test
+	public void testBar() throws Exception {
+		assertThat(bar).isEqualTo("bar");
+	}
+
 	@Test
 	public void testBaz() throws Exception {
-		assertThat(bar).isEqualTo("bar");
+		when(baz.size()).thenReturn(2);
+		assertThat(baz.size()).isEqualTo(2);
 	}
 }
