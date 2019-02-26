e922bbd6e2ac7ddd8c0ce1461117ecfd0f2f9a83
Frank Oh <frankyoh@tmon.co.kr>
2019-02-26 18:36:43 +0900

    added rabbitmq-amqp-client

---
+++ rabbitmq-amqp-client/src/main/java/com/advenoh/NewTask.java
@@ -0,0 +1,28 @@
+package com.advenoh;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
