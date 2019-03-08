86e994886d7680d58484d3f454a96f1c9b923ec0
Frank Oh <frankyoh@tmon.co.kr>
2019-03-08 11:31:44 +0900

    studying rabbitmq - javaconfig hello world working 버전

---
+++ spring-rabbitmq-javaconfig/src/main/java/com/advenoh/config/RabbitMQConfig.java
@@ -0,0 +1,99 @@
+package com.advenoh.config;
+
+import com.advenoh.queue.simple.Receiver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Slf4j
+@Configuration
+@PropertySource("classpath:application.properties")
+public class RabbitMQConfig {
+
+	@Value("${rabbitmq.queue.name}")
+	public String queueName;
+
+	@Value("${rabbitmq.exchange.name}")
+	public String exchangeName;
+
+	@Value("${rabbitmq.host}")
+	public String host;
+
+	@Value("${rabbitmq.port}")
+	public int port;
+
+	@Value("${rabbitmq.username}")
+	public String username;
+
+	@Value("${rabbitmq.password}")
+	public String password;
+
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("concretepage-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
+	}
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+		connectionFactory.setPort(port);
+		connectionFactory.setUsername(username);
+		connectionFactory.setPassword(password);
+		return connectionFactory;
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate() {
+		return new RabbitTemplate(connectionFactory());
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMsg");
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
---
+++ spring-rabbitmq-javaconfig/src/main/java/com/advenoh/queue/simple/Sender.java
@@ -0,0 +1,26 @@
+package com.advenoh.queue.simple;
+
+import com.advenoh.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+public class Sender {
+	final static String queueName = "test.queue";
+
+	public static void main(String[] args) throws InterruptedException {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(RabbitMQConfig.class);
+		ctx.refresh();
+
+		System.out.println("---Message is being sent---");
+		RabbitTemplate rabbitTemplate = (RabbitTemplate) ctx.getBean("rabbitTemplate");
+		Receiver receiver = (Receiver) ctx.getBean("receiver");
+
+		rabbitTemplate.convertAndSend(queueName, "Hello World!");
+		receiver.getCountDownLatch().await(3, TimeUnit.SECONDS);
+
+		ctx.close();
+	}
+}
