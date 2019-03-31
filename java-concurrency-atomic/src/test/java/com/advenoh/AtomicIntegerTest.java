43241a2db98fa0261cf28a8bfa24f9ae50ed2b57
Frank <frankyoh@tmon.co.kr>
2019-03-31 17:03:00 +0900

    added atomic integer example

--- java-concurrency-atomic/src/test/java/com/advenoh/AtomicIntegerTest.java
+++ java-concurrency-atomic/src/test/java/com/advenoh/AtomicIntegerTest.java
@@ -6,41 +6,83 @@ import org.junit.Test;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
 
 import static com.advenoh.ConcurrentUtils.stop;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Slf4j
 public class AtomicIntegerTest {
 	@Test
-	public void test_atomic() {
+	public void test_atomic_incrementAndGet() {
+		int MAX = 10;
 		AtomicInteger atomicInt = new AtomicInteger(0);
-
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 
-		IntStream.range(0, 1000)
-				.forEach(i -> executor.submit(atomicInt::incrementAndGet));
+		IntStream.range(0, MAX)
+				.forEach(i -> {
+					Runnable task = () -> {
+						log.info("threadId:{} - i:{} - value:{}", Thread.currentThread().getId(), i, atomicInt.get());
+						atomicInt.incrementAndGet();
+					};
+					executor.submit(task);
+				});
 
 		stop(executor);
-
-		System.out.println(atomicInt.get());    // => 1000
+		assertThat(atomicInt.get()).isEqualTo(MAX); //1000
 	}
 
 	@Test
-	public void name() {
+	public void test_atomic_updateAndGet() {
+		int MAX = 10;
 		AtomicInteger atomicInt = new AtomicInteger(0);
-
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 
-		IntStream.range(0, 1000)
+		IntStream.range(0, MAX)
 				.forEach(i -> {
-					Runnable task = () ->
-							atomicInt.updateAndGet(n -> n + 2);
+					Runnable task = () -> {
+						log.info("threadId:{} - i:{} - value:{}", Thread.currentThread().getId(), i, atomicInt.get());
+						atomicInt.updateAndGet(n -> n + 2);
+					};
+					executor.submit(task);
+				});
+
+		stop(executor);
+		assertThat(atomicInt.get()).isEqualTo(MAX * 2); //1000
+	}
+
+	@Test
+	public void test_atomic_accumulateAndGet() {
+		int MAX = 10;
+		AtomicInteger atomicInt = new AtomicInteger(0);
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		IntStream.range(0, MAX)
+				.forEach(i -> {
+					Runnable task = () -> {
+						log.info("threadId:{} - i:{} - value:{}", Thread.currentThread().getId(), i, atomicInt.get());
+						atomicInt.accumulateAndGet(i, (n, m) -> n + m);
+					};
 					executor.submit(task);
 				});
 
 		stop(executor);
 
-		System.out.println(atomicInt.get());    // => 2000
+		System.out.println(atomicInt.get());    // => 499500
+	}
+
+	@Test
+	public void test_LongAdder() {
+		int MAX = 1000;
+		LongAdder adder = new LongAdder();
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		IntStream.range(0, MAX)
+				.forEach(i -> executor.submit(adder::increment));
+
+		stop(executor);
+
+		assertThat(adder.sumThenReset()).isEqualTo(MAX);   // => 1000
 	}
 }
