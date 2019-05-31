431e8c9d21c066ebfad22a5ae844b4ebcd1a2f11
Frank Oh <frankyoh@tmon.co.kr>
2019-05-31 12:31:29 +0900

    java8 stream 예제 추가함

---
+++ java8/src/main/java/com/advenoh/SchedulerType.java
@@ -0,0 +1,9 @@
+package com.advenoh;
+
+public enum SchedulerType {
+	ALL,
+	LIVE_ID3,
+	LIVE_SENDBIRD_STATS,
+	LIVE_DEAL_PURCHASE,
+	LIVE_METACOUNTER
+}
--- java8/src/test/java/com/advenoh/Java8Test.java
+++ java8/src/test/java/com/advenoh/Java8Test.java
@@ -1,5 +1,36 @@
 package com.advenoh;
 
-public class Java8Test {
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class Java8Test {
+	@Test
+	public void test_stream으로_getResult_메서드의_총_결과를_반환하는_예제() {
+		boolean result;
+		result = Stream.of(SchedulerType.values())
+				.filter(s1 -> s1 != SchedulerType.ALL)
+				.allMatch(st -> getResult(st));
+		assertThat(result).isFalse();
+		log.info("-------------------------------------------------");
+
+		result = Stream.of(SchedulerType.values())
+				.filter(s1 -> s1 != SchedulerType.ALL)
+				.filter(s1 -> s1 != SchedulerType.LIVE_DEAL_PURCHASE)
+				.allMatch(st -> getResult(st));
+		assertThat(result).isTrue();
+	}
+
+	private boolean getResult(SchedulerType schedulerType) {
+		boolean result = true;
+		if (schedulerType == SchedulerType.LIVE_DEAL_PURCHASE) {
+			result = false;
+		}
+		log.info("schedulerType : {} result : {}", schedulerType, result);
+		return result;
+	}
 }
