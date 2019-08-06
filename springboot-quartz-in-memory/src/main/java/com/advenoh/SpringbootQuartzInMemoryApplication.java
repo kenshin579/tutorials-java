18508ac737b346986c2f2585f78202512e067722
frankyoh <frankyoh@tmon.co.kr>
2019-08-06 20:03:20 +0900

    added springboot-quartz-in-memory module

---
+++ springboot-quartz-in-memory/src/main/java/com/advenoh/SpringbootQuartzInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.advenoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuartzInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuartzInMemoryApplication.class, args);
+	}
+
+}
