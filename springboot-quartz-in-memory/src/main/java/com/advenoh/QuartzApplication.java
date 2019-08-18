842b523964b31f7ae00efe48d17be212160a1946
frankyoh <frankyoh@tmon.co.kr>
2019-08-18 20:30:26 +0900

    rename file

--- springboot-quartz-in-memory/src/main/java/com/advenoh/QuartzApplication.java
+++ springboot-quartz-in-memory/src/main/java/com/advenoh/QuartzApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootQuartzApplication {
+public class QuartzApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootQuartzApplication.class, args);
+		SpringApplication.run(QuartzApplication.class, args);
 	}
 }
