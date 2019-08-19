3d15471d5be023ae6b1609cb8b93679c8fe9ec95
frankyoh <frankyoh@tmon.co.kr>
2019-08-19 20:55:15 +0900

    adding job history entity

--- springboot-quartz-in-memory/src/main/java/com/advenoh/model/JobHistory.java
+++ springboot-quartz-in-memory/src/main/java/com/advenoh/model/JobHistory.java
@@ -1,4 +1,27 @@
 package com.advenoh.model;
 
-public class JobHistory {
+import com.advenoh.model.audit.DateAudit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "job_history")
+@NoArgsConstructor
+public class JobHistory extends DateAudit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String jobName;
+	private String jobGroup;
+	//	private
 }
