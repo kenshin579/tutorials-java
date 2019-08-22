a0c9e64aafdb6d1212d9b2dd16ae67533d89b46b
frankyoh <frankyoh@tmon.co.kr>
2019-08-22 13:58:05 +0900

    unit test 작성중

---
+++ springboot-quartz-in-memory/src/test/java/com/advenoh/service/impl/ScheduleServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.advenoh.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class ScheduleServiceImplTest {
+	@Test
+	public void getAllJobs() {
+
+	}
+
+	@Test
+	public void isJobExists() {
+
+	}
+
+	@Test
+	public void addJob() {
+
+	}
+
+	@Test
+	public void deleteJob() {
+
+	}
+
+	@Test
+	public void pauseJob() {
+
+	}
+
+	@Test
+	public void resumeJob() {
+
+	}
+
+	@Test
+	public void getJobState() {
+
+	}
+}
