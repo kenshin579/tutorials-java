2684162fa20679e6858a587a22c690f7b7269c08
frankyoh <frankyoh@tmon.co.kr>
2019-08-25 14:14:20 +0900

    added example for extracting

---
+++ assertj-unit-test/src/main/java/model/Person.java
@@ -0,0 +1,18 @@
+package model;
+
+import enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Person {
+    private String name;
+    private Gender gender;
+}
--- assertj-unit-test/src/test/java/com/java/examples/AssertJTest.java
+++ assertj-unit-test/src/test/java/com/java/examples/AssertJTest.java
@@ -1,10 +1,28 @@
 package com.java.examples;
 
+import model.Person;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 
+import java.util.List;
+
+import static enums.Gender.FEMALE;
+import static enums.Gender.MALE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AssertJTest {
+	/**
+	 * https://mhaligowski.github.io/blog/2014/09/13/new-extracting.html
+	 * https://github.com/joel-costigliola/assertj-examples/blob/master/assertions-examples/src/test/java/org/assertj/examples/IterableAssertionsExamples.java
+	 */
 	@Test
-	public void test_기본() {
+	public void test_extracting() {
+		Person wilma = new Person("wilma", FEMALE);
+		Person fred = new Person("fred", MALE);
+
+		List<Person> persons = Lists.newArrayList(wilma, fred); // guava-style
+
+		assertThat(persons).extracting("gender").containsExactly(FEMALE, MALE);
 
 	}
 }
