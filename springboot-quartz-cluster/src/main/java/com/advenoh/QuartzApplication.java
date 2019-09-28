d0602c592734a30ee6fc947bbe62770e3e104cf7
frankyoh <frankyoh@tmon.co.kr>
2019-09-28 19:14:42 +0900

    Jpa JavaConfig 분리

--- springboot-quartz-cluster/src/main/java/com/advenoh/QuartzApplication.java
+++ springboot-quartz-cluster/src/main/java/com/advenoh/QuartzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class QuartzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzApplication.class, args);
---
+++ springboot-quartz-cluster/src/main/java/com/advenoh/spring/config/JpaDatabaseConfig.java
@@ -0,0 +1,12 @@
+package com.advenoh.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * https://stackoverflow.com/questions/51467132/spring-webmvctest-with-enablejpa-annotation
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaDatabaseConfig {
+}
