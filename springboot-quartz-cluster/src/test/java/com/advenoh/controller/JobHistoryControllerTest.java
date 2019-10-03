c6eea584e57847248389d8f724eb85a39c72a028
frankyoh <frankyoh@tmon.co.kr>
2019-10-04 08:02:17 +0900

    JobHistoryController에 대한 unit test 추가

--- springboot-quartz-cluster/src/test/java/com/advenoh/controller/JobHistoryControllerTest.java
+++ springboot-quartz-cluster/src/test/java/com/advenoh/controller/JobHistoryControllerTest.java
@@ -1,52 +1,51 @@
-//package com.advenoh.controller;
-//
-//import com.advenoh.job.SimpleJob;
-//import com.advenoh.service.JobHistoryService;
-//import com.advenoh.service.ScheduleService;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.*;
-//import static org.mockito.ArgumentMatchers.anyObject;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@Slf4j
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(JobHistoryController.class)
-//public class JobHistoryControllerTest {
-//    private final String BASE_PATH = "/scheduler";
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @MockBean
-//    private JobHistoryService jobHistoryService;
-//
-//    @Test
-//    public void getAllHistoryJobs() {
-//        given(jobHistoryService.getAllJobs(anyObject()).;
-//        given(scheduleService.isJobExists(anyObject())).willReturn(false);
-//
-//        mvc.perform(post(BASE_PATH + "/job")
-//                .param("jobName", "job1")
-//                .param("jobGroup", "testGroup"))
-//                .andDo(print())
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.success", is(true)));
-//
-//        verify(scheduleService).addJob(anyObject(), eq(SimpleJob.class));
-//        verify(scheduleService).isJobExists(anyObject());
-//    }
-//}
+package com.advenoh.controller;
+
+import com.advenoh.model.JobStatus;
+import com.advenoh.service.JobHistoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyObject;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@WebMvcTest(JobHistoryController.class)
+public class JobHistoryControllerTest {
+    private final String BASE_PATH = "/history";
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private JobHistoryService jobHistoryService;
+
+    @Test
+    public void getAllHistoryJobs() throws Exception {
+        List<JobStatus> jobStatusList = new ArrayList<>();
+        Page<JobStatus> jobStatusPage = new PageImpl<>(jobStatusList);
+        given(jobHistoryService.getAllJobs(anyObject())).willReturn(jobStatusPage);
+
+        this.mvc.perform(get(BASE_PATH + "/jobs"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray());
+
+        verify(jobHistoryService).getAllJobs(anyObject());
+    }
+}
