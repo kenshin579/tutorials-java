3b37dc32122c2cc96e0ffbeb4128a728719df515
frankyoh <frankyoh@tmon.co.kr>
2019-10-22 14:21:59 +0900

    일대다 init 추가

---
+++ springboot-jpa-one-to-many-undirectional/README.md
@@ -0,0 +1,6 @@
+Spring Boot Quartz - 메모리기반
+======
+Spring Boot Quartz 관련 코드
+
+## 블로그
+- <a href="https://advenoh.tistory.com/52" target="_blank">Spring Boot + Quartz을 이용한 Job Scheduler 구현 (In-memory)</a>
---
+++ springboot-jpa-one-to-many-undirectional/src/main/java/com/advenoh/controller/QuizController.java
@@ -0,0 +1,11 @@
+package com.advenoh.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+}
