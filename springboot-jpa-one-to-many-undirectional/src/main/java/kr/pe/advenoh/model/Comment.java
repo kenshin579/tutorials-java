6f54b27afa2251382a82e232866346faef274ad3
frankyoh <frankyoh@tmon.co.kr>
2019-11-21 19:00:39 +0900

    업데이팅

--- springboot-jpa-one-to-many-undirectional/src/main/java/kr/pe/advenoh/model/Comment.java
+++ springboot-jpa-one-to-many-undirectional/src/main/java/kr/pe/advenoh/model/Comment.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Comment extends DateAudit {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "author")
 	private String author;
 
 	@Lob
