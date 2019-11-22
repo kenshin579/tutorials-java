5a59b3e11202f27e148876afe19b29aad25328f3
frankyoh <frankyoh@tmon.co.kr>
2019-11-22 17:29:52 +0900

    새로운 모듈 추가함

---
+++ springboot-jpa-unit-test/src/main/java/kr/pe/advenoh/JpaUnitTestApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootjpaunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUnitTestApplication.class, args);
+	}
+
+}
