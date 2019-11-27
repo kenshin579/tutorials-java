7ae5b8fbaa3d04ff284da6ce8729fb932342a50d
frankyoh <frankyoh@tmon.co.kr>
2019-11-27 23:48:41 +0900

    새로운 모듈 추가함

---
+++ springboot-jpa-many-to-many-undirectional/src/main/java/kr/pe/advenoh/springbootjpamanytomanyundirectional/SpringbootJpaManyToManyUndirectionalApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootjpamanytomanyundirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaManyToManyUndirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaManyToManyUndirectionalApplication.class, args);
+	}
+
+}
---
+++ springboot-jpa-many-to-many-undirectional/src/test/java/kr/pe/advenoh/springbootjpamanytomanyundirectional/SpringbootJpaManyToManyUndirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootjpamanytomanyundirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaManyToManyUndirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
