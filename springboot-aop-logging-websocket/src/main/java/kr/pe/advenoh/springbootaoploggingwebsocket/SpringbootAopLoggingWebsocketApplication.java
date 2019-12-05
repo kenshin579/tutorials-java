0629b11c0f76dd9b1f3871d308ac15de6174a60d
frankyoh <frankyoh@tmon.co.kr>
2019-12-05 18:34:29 +0900

    aop 로그 모듈 추가

---
+++ springboot-aop-logging-websocket/src/main/java/kr/pe/advenoh/springbootaoploggingwebsocket/SpringbootAopLoggingWebsocketApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootaoploggingwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopLoggingWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopLoggingWebsocketApplication.class, args);
+	}
+
+}
