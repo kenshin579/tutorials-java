f9dfc83896dd92049aef08398d271b7e9f464600
frankyoh <frankyoh@tmon.co.kr>
2019-12-05 18:34:29 +0900

    aop 로그 모듈 추가

---
+++ springboot-aop-logging-websocket/src/test/java/kr/pe/advenoh/springbootaoploggingwebsocket/SpringbootAopLoggingWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootaoploggingwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopLoggingWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
