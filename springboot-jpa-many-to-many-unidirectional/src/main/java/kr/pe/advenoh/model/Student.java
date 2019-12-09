b0154b306c39be3f34bb5b16cd146fb7ac25a0fa
frankyoh <frankyoh@tmon.co.kr>
2019-12-09 22:09:47 +0900

    rename 모듈

--- springboot-jpa-many-to-many-unidirectional/src/main/java/kr/pe/advenoh/model/Student.java
+++ springboot-jpa-many-to-many-unidirectional/src/main/java/kr/pe/advenoh/model/Student.java
@@ -7,17 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
