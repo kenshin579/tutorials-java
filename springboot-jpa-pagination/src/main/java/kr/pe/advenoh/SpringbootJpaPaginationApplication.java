1e9aee7ab00d42bb45a115b02e05c9c2d62e0a8f
frankyoh <frankyoh@tmon.co.kr>
2019-12-10 10:57:28 +0900

    jpa pagination 추가함

---
+++ springboot-jpa-pagination/src/main/java/kr/pe/advenoh/SpringbootJpaPaginationApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaPaginationApplication.class, args);
+	}
+
+}
