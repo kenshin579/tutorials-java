d0d5cc7138750ce2334a3ea0c3d63bd2baeff356
frankyoh <frankyoh@tmon.co.kr>
2019-12-11 22:48:36 +0900

    jpa n+1 problem 모듈 추가함

---
+++ springboot-jpa-n1-problem/src/main/java/kr/pe/advenoh/springbootjpan1problem/SpringbootJpaN1ProblemApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootjpan1problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaN1ProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaN1ProblemApplication.class, args);
+	}
+
+}
---
+++ springboot-jpa-n1-problem/src/test/java/kr/pe/advenoh/springbootjpan1problem/SpringbootJpaN1ProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh.springbootjpan1problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaN1ProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
