697de388194b90ece5a14731b641346f3f61094b
frankyoh <frankyoh@tmon.co.kr>
2019-12-18 16:32:00 +0900

    타입별로 List생성하기

---
+++ java8/src/main/java/com/advenoh/vod/VodCollection.java
@@ -0,0 +1,21 @@
+package com.advenoh.vod;
+
+import com.advenoh.vod.enums.VodCollectionType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VodCollection {
+	private String title;
+	private VodCollectionType vodCollectionType;
+	private String content;
+
+	@Builder
+	public VodCollection(String title, VodCollectionType vodCollectionType, String content) {
+		this.title = title;
+		this.vodCollectionType = vodCollectionType;
+		this.content = content;
+	}
+}
