a36dee469dd9ae92172eacaf790d9c306398c584
frankyoh <frankyoh@tmon.co.kr>
2020-03-25 19:29:33 +0900

    pages 추가 및 불필요한 파일 삭제

---
+++ springboot-quartz-cluster-reactjs/app/src/pages/ListPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ListPage = () => {
+
+    return (
+        <div>ListPage</div>
+    );
+};
+
+export default ListPage;
---
+++ springboot-quartz-cluster-reactjs/app/src/pages/index.js
@@ -0,0 +1,2 @@
+export {default as ListPage} from './ListPage';
+export {default as NotFoundPage} from './NotFoundPage';
