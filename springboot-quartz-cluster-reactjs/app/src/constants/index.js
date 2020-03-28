b756c5be0c5d1591b502a10aa792ee402e0242cc
frankyoh <frankyoh@tmon.co.kr>
2020-03-28 23:06:01 +0900

    사용하지 않는 상수 삭제

--- springboot-quartz-cluster-reactjs/app/src/constants/index.js
+++ springboot-quartz-cluster-reactjs/app/src/constants/index.js
@@ -1,7 +1,4 @@
-export const API_SCHEDULE_BASE_URL = '/scheduler';
-export const API_HISTORY_BASE_URL = '/history';
 export const DATE_TIME_FORMAT = 'YYYY-MM-DD h:mm:ss';
-export const ACCESS_TOKEN = 'accessToken';
 export const MAX_INTERVAL_SECONDS = 15000;
 export const DELAY_TIME_FOR_MESSAGE = 3000;
 
