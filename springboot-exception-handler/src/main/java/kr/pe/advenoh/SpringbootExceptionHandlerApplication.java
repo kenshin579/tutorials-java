ffc3788b215cbc1a2048785be1a2aadc36a702c4
frankyoh <frankyoh@tmon.co.kr>
2020-07-30 18:13:06 +0900

    [TUT-35] exception handler init commit

---
+++ springboot-exception-handler/src/main/java/kr/pe/advenoh/SpringbootExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExceptionHandlerApplication.class, args);
+	}
+
+}
