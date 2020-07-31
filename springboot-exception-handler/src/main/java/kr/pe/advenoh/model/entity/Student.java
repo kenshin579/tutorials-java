8ea1059701a8324d1bb9013d2d4a4c528d263947
frankyoh <frankyoh@tmon.co.kr>
2020-07-31 20:42:00 +0900

    [TUT-35] entity 추가

---
+++ springboot-exception-handler/src/main/java/kr/pe/advenoh/model/entity/Student.java
@@ -0,0 +1,32 @@
+package kr.pe.advenoh.model.entity;
+
+import kr.pe.advenoh.model.audit.DateAudit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Student extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id", unique = true, nullable = false)
+    private Long id;
+
+    private String name;
+    private String mobileNumber;
+
+    private String address;
+
+
+}
