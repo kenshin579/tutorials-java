d924f85ad929881bbbe96ca3d5b8f50f939a3baa
frankyoh <frankyoh@tmon.co.kr>
2020-07-31 20:42:17 +0900

    [TUT-35] date audit 파일 수정

--- springboot-exception-handler/src/main/java/kr/pe/advenoh/model/audit/DateAudit.java
+++ springboot-exception-handler/src/main/java/kr/pe/advenoh/model/audit/DateAudit.java
@@ -3,6 +3,7 @@ package kr.pe.advenoh.model.audit;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -18,4 +19,8 @@ public abstract class DateAudit {
     @CreatedDate
     @Column(name = "create_dt", nullable = false, updatable = false)
     private LocalDateTime createDt;
+
+    @LastModifiedDate
+    @Column(name = "updated_dt", nullable = false)
+    private LocalDateTime updateDt;
 }
