5000fa10d54698ed9b7a955c4243ddc8a9f64f7a
frankyoh <frankyoh@tmon.co.kr>
2020-08-09 16:20:59 +0900

    [TUT-35] 사용하지 않는 파일 삭제

--- springboot-exception-handler/src/main/java/kr/pe/advenoh/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.pe.advenoh.repository;
-
-import kr.pe.advenoh.model.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
