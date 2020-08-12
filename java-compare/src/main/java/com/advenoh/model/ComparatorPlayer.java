2f09c9d1d92374f7d50e5e06e7be083f044f5d06
frankyoh <frankyoh@tmon.co.kr>
2020-08-12 14:46:37 +0900

    [TUT-39] comparable, comparator unit test 추가

---
+++ java-compare/src/main/java/com/advenoh/model/ComparatorPlayer.java
@@ -0,0 +1,13 @@
+package com.advenoh.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ComparatorPlayer {
+	private String name;
+	private int score;
+}
---
+++ java-compare/src/test/java/com/advenoh/CompareTest.java
@@ -0,0 +1,57 @@
+package com.advenoh;
+
+import com.advenoh.model.ComparablePlayer;
+import com.advenoh.model.ComparatorPlayer;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CompareTest {
+
+	@Test
+	public void comparableTest() {
+		List<ComparablePlayer> players = new ArrayList<>();
+		players.add(new ComparablePlayer("Alice", 899));
+		players.add(new ComparablePlayer("Bob", 982));
+		players.add(new ComparablePlayer("Chloe", 1090));
+		players.add(new ComparablePlayer("Dale", 982));
+		players.add(new ComparablePlayer("Eric", 1018));
+
+		Comparator<ComparablePlayer> comparator = new Comparator<ComparablePlayer>() {
+			@Override
+			public int compare(ComparablePlayer a, ComparablePlayer b) {
+				return b.getScore() - a.getScore();
+			}
+		};
+
+		Collections.sort(players);
+
+		assertThat(players).isSortedAccordingTo(comparator);
+	}
+
+	@Test
+	public void comparatorTest() {
+		List<ComparatorPlayer> players = new ArrayList<>();
+		players.add(new ComparatorPlayer("Alice", 899));
+		players.add(new ComparatorPlayer("Bob", 982));
+		players.add(new ComparatorPlayer("Chloe", 1090));
+		players.add(new ComparatorPlayer("Dale", 982));
+		players.add(new ComparatorPlayer("Eric", 1018));
+
+		Comparator<ComparatorPlayer> comparator = new Comparator<ComparatorPlayer>() {
+			@Override
+			public int compare(ComparatorPlayer a, ComparatorPlayer b) {
+				return b.getScore() - a.getScore();
+			}
+		};
+
+		Collections.sort(players, comparator);
+
+		assertThat(players).isSortedAccordingTo(comparator);
+	}
+}
