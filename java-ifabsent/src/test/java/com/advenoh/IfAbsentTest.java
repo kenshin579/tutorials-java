9a1116a16c8f8c3457a31618d1aa4251f3a692f3
frankyoh <frankyoh@tmon.co.kr>
2020-08-12 23:10:02 +0900

    [TUT-41] putIfAbsent, computeIfAbsent 예제 추가

---
+++ java-ifabsent/src/test/java/com/advenoh/IfAbsentTest.java
@@ -0,0 +1,87 @@
+package com.advenoh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * http://tech.javacafe.io/2018/12/03/HashMap/
+ */
+@Slf4j
+public class IfAbsentTest {
+    private Map<Integer, BigInteger> memoizeHashMap;
+
+    @Before
+    public void setUp() throws Exception {
+        memoizeHashMap = new HashMap<>() {{
+            put(0, BigInteger.ZERO);
+            put(1, BigInteger.ONE);
+            put(2, BigInteger.ONE);
+        }};
+    }
+
+    @Test
+    public void fibonacci_test() {
+        int max = 10;
+        for (int i = 0; i < max; i++) {
+            log.info("{}", fibonacci(i));
+        }
+    }
+
+    private BigInteger fibonacci(int n) {
+        if (memoizeHashMap.containsKey(n)) {
+            return memoizeHashMap.get(n);
+        } else {
+            BigInteger result = fibonacci(n - 1).add(fibonacci(n - 2));
+            memoizeHashMap.put(n, result);
+            return result;
+        }
+    }
+
+    @Test
+    public void fibonacci_test2() {
+        int max = 10;
+        for (int i = 0; i < max; i++) {
+            log.info("{}", fibonacciComputeIfAbsent(i));
+        }
+    }
+
+    private BigInteger fibonacciComputeIfAbsent(int n) {
+        return memoizeHashMap.computeIfAbsent(n,
+                (key) -> fibonacci(n - 1).add(fibonacci(n - 2)));
+    }
+
+
+    @Test
+    public void computeIfAbsent() {
+        Map<String, Integer> stringLengthMap = new HashMap<>();
+        stringLengthMap.put("John", 5);
+        log.info("stringLengthMap1 : {}", stringLengthMap);
+
+        assertThat(stringLengthMap.computeIfAbsent("John", key -> key.length())).isEqualTo(5); //존재하면 value값을 반환함
+        log.info("stringLengthMap2 : {}", stringLengthMap);
+
+        //없으면 2번째 인자 함수를 실행한 결과를 반환하고 map에도 추가가 된다
+        assertThat(stringLengthMap.computeIfAbsent("JohnNon", key -> key.length())).isEqualTo("JohnNon".length());
+        assertThat(stringLengthMap.get("JohnNon")).isNotNull();
+        log.info("stringLengthMap3 : {}", stringLengthMap);
+    }
+
+    @Test
+    public void putIfAbsent() {
+        Map<String, Integer> stringLengthMap = new HashMap<>();
+        stringLengthMap.put("John", 5);
+
+        assertThat(stringLengthMap.putIfAbsent("John", 10)).isEqualTo(5); //존재하는 경우, value값을 반환한다
+        assertThat(stringLengthMap.size()).isEqualTo(1);
+
+        assertThat(stringLengthMap.putIfAbsent("JohnNon", 10)).isNull(); //없는 경우, null로 반환하고 map에 저장함
+        assertThat(stringLengthMap.size()).isEqualTo(2);
+    }
+}
