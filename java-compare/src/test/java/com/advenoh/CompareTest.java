57c86b7f462f770d345bfeb749a38a2a115e42d7
frankyoh <frankyoh@tmon.co.kr>
2020-08-12 15:43:17 +0900

    [TUT-39] comparingThen 예제 추가

--- java-compare/src/test/java/com/advenoh/CompareTest.java
+++ java-compare/src/test/java/com/advenoh/CompareTest.java
@@ -17,36 +17,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 public class CompareTest {
 	List<ComparablePlayer> comparablePlayers;
+	List<ComparatorPlayer> comparatorPlayers;
 
 	@Before
 	public void setUp() {
 		comparablePlayers = new ArrayList<>();
+		comparablePlayers.add(new ComparablePlayer("Dale", 982));
+		comparablePlayers.add(new ComparablePlayer("Chloe", 1090));
 		comparablePlayers.add(new ComparablePlayer("Alice", 899));
 		comparablePlayers.add(new ComparablePlayer("Bob", 982));
-		comparablePlayers.add(new ComparablePlayer("Chloe", 1090));
-		comparablePlayers.add(new ComparablePlayer("Dale", 982));
 		comparablePlayers.add(new ComparablePlayer("Eric", 1018));
+
+		comparatorPlayers = new ArrayList<>();
+		comparatorPlayers.add(new ComparatorPlayer("Dale", 982));
+		comparatorPlayers.add(new ComparatorPlayer("Bob", 982));
+		comparatorPlayers.add(new ComparatorPlayer("Eric", 1018));
+		comparatorPlayers.add(new ComparatorPlayer("Chloe", 1090));
+		comparatorPlayers.add(new ComparatorPlayer("Alice", 899));
 	}
 
 	/**
 	 * https://www.daleseo.com/java-comparable-comparator/
 	 */
 	@Test
-	public void comparableTest() {
+	public void comparable_test() {
 		Collections.sort(comparablePlayers);
 
 		assertThat(comparablePlayers).isSorted();
 	}
 
 	@Test
-	public void comparatorTest() {
-		List<ComparatorPlayer> players = new ArrayList<>();
-		players.add(new ComparatorPlayer("Alice", 899));
-		players.add(new ComparatorPlayer("Bob", 982));
-		players.add(new ComparatorPlayer("Chloe", 1090));
-		players.add(new ComparatorPlayer("Dale", 982));
-		players.add(new ComparatorPlayer("Eric", 1018));
-
+	public void comparator_test() {
 		Comparator<ComparatorPlayer> comparator = new Comparator<ComparatorPlayer>() {
 			@Override
 			public int compare(ComparatorPlayer a, ComparatorPlayer b) {
@@ -54,30 +55,22 @@ public class CompareTest {
 			}
 		};
 
-		Collections.sort(players, comparator);
+		Collections.sort(comparatorPlayers, comparator);
 
-		assertThat(players).isSortedAccordingTo(comparator);
+		assertThat(comparatorPlayers).isSortedAccordingTo(comparator);
 	}
 
 	@Test
 	public void comparator_lambda_Test() {
-		List<ComparatorPlayer> players = new ArrayList<>();
-		players.add(new ComparatorPlayer("Alice", 899));
-		players.add(new ComparatorPlayer("Bob", 982));
-		players.add(new ComparatorPlayer("Chloe", 1090));
-		players.add(new ComparatorPlayer("Dale", 982));
-		players.add(new ComparatorPlayer("Eric", 1018));
-
 		Comparator<ComparatorPlayer> comparator = (a, b) -> b.getScore() - a.getScore();
 
-		Collections.sort(players, comparator);
+		Collections.sort(comparatorPlayers, comparator);
 
-		assertThat(players).isSortedAccordingTo(comparator);
+		assertThat(comparatorPlayers).isSortedAccordingTo(comparator);
 	}
 
-
 	@Test
-	public void streamComparableSort() {
+	public void stream_comparable_sort() {
 		List<ComparablePlayer> sortedPlayers = comparablePlayers.stream()
 				.sorted((a, b) -> b.getScore() - a.getScore())
 				.collect(Collectors.toList());
@@ -86,20 +79,33 @@ public class CompareTest {
 	}
 
 	@Test
-	public void sortByScoreThenName() {
-		List<ComparatorPlayer> players = new ArrayList<>();
-		players.add(new ComparatorPlayer("Chloe", 1090));
-		players.add(new ComparatorPlayer("Dale", 982));
-		players.add(new ComparatorPlayer("Alice", 899));
-		players.add(new ComparatorPlayer("Bob", 982));
-		players.add(new ComparatorPlayer("Eric", 1018));
-
-		List<ComparatorPlayer> sortedPlayers = players.stream()
+	public void sort_by_score_then_name() {
+		List<ComparatorPlayer> sortedPlayers = comparatorPlayers.stream()
 				//.sorted((a, b) -> ComparatorPlayer.compareByNameThenScore(a, b))
 				.sorted(ComparatorPlayer::compareByScoreThenName)
 				.collect(Collectors.toList());
 
 		assertThat(sortedPlayers).isSortedAccordingTo(ComparatorPlayer::compareByScoreThenName);
 
+	}
+
+	@Test
+	public void sort_extracted_comparators_test() {
+		List<ComparatorPlayer> sortedPlayers = comparatorPlayers.stream()
+//				.sorted(Comparator.comparing(a -> a.getScore()))
+				.sorted(Comparator.comparing(ComparatorPlayer::getScore))
+				.collect(Collectors.toList());
+
+		assertThat(sortedPlayers).isSortedAccordingTo(Comparator.comparing(ComparatorPlayer::getScore));
+	}
+
+	@Test
+	public void thenComparing_test() {
+		List<ComparatorPlayer> sortedPlayers = comparatorPlayers.stream()
+				.sorted(Comparator.comparing(ComparatorPlayer::getScore)
+						.thenComparing(ComparatorPlayer::getName))
+				.collect(Collectors.toList());
+
+		assertThat(sortedPlayers).isSortedAccordingTo(ComparatorPlayer::compareByScoreThenName);
 	}
 }
