3703e40abadae85fed05d0ceb4893f5e1b7c29ca
kenshin579 <kenshin579@hotmail.com>
2020-09-05 21:21:24 +0900

    [TUT-56] mustache 확잔자 html로 하면 변수값이 치환이 안되는 이슈 - JavaConfig 설정으로 변경함

--- springboot-whitelabel-error-page/src/main/java/kr/pe/advenoh/controller/ViewMappingController.java
+++ springboot-whitelabel-error-page/src/main/java/kr/pe/advenoh/controller/ViewMappingController.java
@@ -5,13 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ViewMappingController {
-    @GetMapping({"/", "/index"})
-    public String index(Model model) {
-        return "index";
+    @GetMapping
+    public ModelAndView index(Model model) {
+        model.addAttribute("name", "Frank");
+        return new ModelAndView("index");
     }
 }
