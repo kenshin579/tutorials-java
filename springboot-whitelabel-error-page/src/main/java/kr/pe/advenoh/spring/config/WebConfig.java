ed45326d7f344e07179ed375c1d3640b638e0c0e
kenshin579 <kenshin579@hotmail.com>
2020-09-05 21:21:24 +0900

    [TUT-56] mustache 확잔자 html로 하면 변수값이 치환이 안되는 이슈 - JavaConfig 설정으로 변경함

---
+++ springboot-whitelabel-error-page/src/main/java/kr/pe/advenoh/spring/config/WebConfig.java
@@ -0,0 +1,21 @@
+package kr.pe.advenoh.spring.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
+    }
+}
