ea9431d7d429bc0cd46f2fed39877c1a84978dee
kenshin579 <kenshin579@hotmail.com>
2020-09-06 10:44:36 +0900

    [TUT-56] CustomErrorController 추가

---
+++ springboot-whitelabel-error-page/src/main/java/kr/pe/advenoh/controller/CustomErrorController.java
@@ -0,0 +1,32 @@
+package kr.pe.advenoh.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "errors/404-custom";
+            }
+        }
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
