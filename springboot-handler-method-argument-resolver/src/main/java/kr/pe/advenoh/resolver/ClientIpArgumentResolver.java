7b5134905e94202fce98829a057963fb0c304045
kenshin579 <kenshin579@hotmail.com>
2020-09-09 12:03:50 +0900

    [TUT-62] Argument Resolver 추가

---
+++ springboot-handler-method-argument-resolver/src/main/java/kr/pe/advenoh/resolver/ClientIpArgumentResolver.java
@@ -0,0 +1,33 @@
+package kr.pe.advenoh.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ClientIpArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasParameterAnnotation(ClientIp.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) nativeWebRequest.getNativeRequest();
+
+        String clientIp = request.getHeader("X-Forwarded-For");
+        if (StringUtils.isEmpty(clientIp) || "unknown".equalsIgnoreCase(clientIp)) {
+            clientIp = request.getRemoteAddr();
+        }
+        log.debug("[debug] clientIp : {}", clientIp);
+        return clientIp;
+    }
+}
