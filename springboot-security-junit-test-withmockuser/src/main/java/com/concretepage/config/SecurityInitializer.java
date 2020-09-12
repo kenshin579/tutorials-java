245ebe05cf1b951978e7d4f0c95f88e7f88be744
kenshin579 <kenshin579@hotmail.com>
2020-09-12 13:16:19 +0900

    [TUT-67] gradle 모듈 삭제함

--- springboot-security-junit-test-withmockuser/src/main/java/com/concretepage/config/SecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.concretepage.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-} 
