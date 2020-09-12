8473f12f690be6f6a6e5d41da2df1722fe4f98c2
kenshin579 <kenshin579@hotmail.com>
2020-09-12 13:36:35 +0900

    spring argument resolver 파일 spring 폴더로 옮기

--- springboot-handler-method-argument-resolver/src/main/java/kr/pe/advenoh/spring/config/WebConfig.java
+++ springboot-handler-method-argument-resolver/src/main/java/kr/pe/advenoh/spring/config/WebConfig.java
@@ -1,6 +1,6 @@
 package kr.pe.advenoh.spring.config;
 
-import kr.pe.advenoh.resolver.ClientIpArgumentResolver;
+import kr.pe.advenoh.spring.resolver.ClientIpArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
--- springboot-handler-method-argument-resolver/src/main/java/kr/pe/advenoh/spring/resolver/ClientIpArgumentResolver.java
+++ springboot-handler-method-argument-resolver/src/main/java/kr/pe/advenoh/spring/resolver/ClientIpArgumentResolver.java
@@ -1,5 +1,6 @@
-package kr.pe.advenoh.resolver;
+package kr.pe.advenoh.spring.resolver;
 
+import kr.pe.advenoh.resolver.ClientIp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
