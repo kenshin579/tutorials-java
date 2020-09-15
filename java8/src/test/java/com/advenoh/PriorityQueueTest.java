da79ab0b1751fb26cc9eec5a2739ae6ff2e8a049
kenshin579 <kenshin579@hotmail.com>
2020-09-16 00:03:02 +0900

    [TUT-49] PriorityQueueTest 추가

---
+++ java8/src/test/java/com/advenoh/PriorityQueueTest.java
@@ -0,0 +1,36 @@
+package com.advenoh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class PriorityQueueTest {
+    @Test
+    public void test_minHeap() {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.add(3);
+        minHeap.add(1);
+        minHeap.add(10);
+        minHeap.add(5);
+
+        assertThat(minHeap.poll()).isEqualTo(1);
+        assertThat(minHeap.poll()).isEqualTo(3);
+    }
+
+    @Test
+    public void test_maxHeap() {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap.add(3);
+        minHeap.add(1);
+        minHeap.add(10);
+        minHeap.add(5);
+
+        assertThat(minHeap.poll()).isEqualTo(10);
+        assertThat(minHeap.poll()).isEqualTo(5);
+    }
+}
