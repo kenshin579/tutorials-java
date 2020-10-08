53cdc3251cbc1d066e6bf8ce33a6b853d112207a
kenshin579 <kenshin579@hotmail.com>
2020-10-08 18:07:22 +0900

    [TJA-49] 파일이름 변경

---
+++ java-collections/src/test/java/com/advenoh/CollectionTest.java
@@ -0,0 +1,21 @@
+package com.advenoh;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CollectionTest {
+	@Test
+	public void test_list() {
+		List list = new ArrayList();
+	}
+
+	@Test
+	public void test_map() {
+		Map map = new HashMap();
+	}
+
+}
