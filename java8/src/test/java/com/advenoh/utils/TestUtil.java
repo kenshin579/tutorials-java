c5b341a5914eac5494f69778785192f9b338e1c9
kenshin579 <kenshin579@hotmail.com>
2020-10-13 11:18:28 +0900

    [TJA-80] TestUtil 추가

---
+++ java8/src/test/java/com/advenoh/utils/TestUtil.java
@@ -0,0 +1,22 @@
+package com.advenoh.utils;
+
+import com.advenoh.Student;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class TestUtil {
+	private TestUtil() {
+	}
+
+	/**
+	 * https://stackoverflow.com/questions/22649978/java-8-lambda-can-i-generate-a-new-arraylist-of-objects-from-an-intstream
+	 * http://jtuts.com/2017/04/21/create-list-range-integers-using-java-8/
+	 */
+	public static List<Student> getStudentSample(int max) {
+		return IntStream.range(0, max)
+				.mapToObj(i -> new Student("name" + i, i + 10))
+				.collect(Collectors.toList());
+	}
+}
