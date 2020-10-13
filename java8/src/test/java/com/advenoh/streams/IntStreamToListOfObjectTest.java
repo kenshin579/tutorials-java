423f2a67abbc05f6798c8ee0f04e7c3817ea12cd
kenshin579 <kenshin579@hotmail.com>
2020-10-13 11:22:57 +0900

    [TJA-80] IntStream -> List of Obj 예제 작성완료

--- java8/src/test/java/com/advenoh/streams/IntStreamToListOfObjectTest.java
+++ java8/src/test/java/com/advenoh/streams/IntStreamToListOfObjectTest.java
@@ -1,4 +1,27 @@
 package com.advenoh.streams;
 
+import com.advenoh.Student;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class IntStreamToListOfObjectTest {
+
+	/**
+	 * https://stackoverflow.com/questions/22649978/java-8-lambda-can-i-generate-a-new-arraylist-of-objects-from-an-intstream
+	 * http://jtuts.com/2017/04/21/create-list-range-integers-using-java-8/
+	 */
+	@Test
+	public void convert_intstream_list_of_obj() {
+		int max = 3;
+		List<Student> students = IntStream.range(0, max)
+				.mapToObj(i -> new Student("name" + i, i + 10))
+				.collect(Collectors.toList());
+
+		assertThat(students.size()).isEqualTo(max);
+	}
 }
