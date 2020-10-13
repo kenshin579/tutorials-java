650edcbf3392836874324c97d5ad7f013ec7d69a
kenshin579 <kenshin579@hotmail.com>
2020-10-13 12:22:32 +0900

    [TJA-49] model 패키지 수정

--- java8/src/main/java/com/advenoh/model/Student.java
+++ java8/src/main/java/com/advenoh/model/Student.java
@@ -1,18 +1,20 @@
-package com.advenoh;
+package com.advenoh.model;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Student {
-    private String name;
-    private int age;
+	private String name;
+	private int age;
 
-    @Builder
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+	@Builder
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
