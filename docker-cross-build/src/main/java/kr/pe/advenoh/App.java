b171d78b0a1f8c5d1942a92fdc959ea44e786e56
kenshin579 <kenshin579@hotmail.com>
2021-07-24 23:56:22 +0900

    [TJA-94] 헬로드 월드 자바 코드 추가

---
+++ docker-cross-build/src/main/java/kr/pe/advenoh/App.java
@@ -0,0 +1,10 @@
+package kr.pe.advenoh;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from " + System.getProperty("os.arch") + " architecture");
+    }
+}
